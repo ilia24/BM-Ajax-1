03348c76e7a2884c62d10a6ab727024fd0960072
ilia24 <iliademertchian24@gmail.com>
2017-05-03 23:48:19 -0400

    Add ajax request to append ping data to section

--- ajax.js
+++ ajax.js
@@ -8,6 +8,19 @@ $('#ajaxbutton').on('click', function(){
     // data: {},
     dataType: 'html'
   });
-})
+});
+
+
+$('#ping').on('click', function(){
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/ping/',
+    method: 'GET',
+    // data: {},
+    dataType: 'html'
+  }).done(function (rdata) {
+    console.log(rdata);
+    $('#step3456').append(rdata);
+  });
+});
 
 });
