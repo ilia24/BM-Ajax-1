7834e14ce0e09952699eb2e4fbd2fbef92b13ea4
ilia24 <iliademertchian24@gmail.com>
2017-05-03 23:51:18 -0400

    Add fail case to ping, remade fail case into its own button

--- ajax.js
+++ ajax.js
@@ -18,8 +18,26 @@ $('#ping').on('click', function(){
     // data: {},
     dataType: 'html'
   }).done(function (rdata) {
-    console.log(rdata);
+    console.log('ajax query succedeed');
     $('#step3456').append(rdata);
+  }).fail(function() {
+    console.log('ajax query failed')
+    $('#step3456').append('Ajax query failed send help!');
+  });
+});
+
+$('#pong').on('click', function(){
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/pong/',
+    method: 'GET',
+    // data: {},
+    dataType: 'html'
+  }).done(function (rdata) {
+    console.log('ajax query succedeed');
+    $('#step3456').append(rdata);
+  }).fail(function() {
+    console.log('ajax query failed')
+    $('#step3456').append('Ajax query failed send help!');
   });
 });
 
